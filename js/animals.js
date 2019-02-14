4d7ba81e7a42dc5472de381bb169075943e4745d
Dorota Kowalska <dorota.d.kowalska@gmail.com>
2019-02-14 20:34:06 +0100

    hodowla page update

--- js/animals.js
+++ js/animals.js
@@ -1,11 +1,30 @@
-const cards = [...document.querySelectorAll(".card")];
 const backgroundFlip = [...document.querySelectorAll(".card>div:nth-of-type(3)")];
 const descriptionFlip = [...document.querySelectorAll(".card>div:nth-of-type(2)")];
 const nameFlip = [...document.querySelectorAll("div.card>p:nth-child(2) ")];
+const cards = [...document.querySelectorAll(".card")];
+
 
 
 $(".card").on('click', function () {
  backgroundFlip[$(this).index()].classList.toggle('bcg-flip');
  descriptionFlip[$(this).index()].classList.toggle('description-flip');
  nameFlip[$(this).index()].classList.toggle('name-flip');
+})
+
+const isInViewport = function () {
+ var elementTop = $(this).offset().top;
+ var elementBottom = elementTop + $(this).outerHeight();
+
+ var viewportTop = $(window).scrollTop();
+ var viewportBottom = viewportTop + $(window).height();
+
+ return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on('resize scroll', function () {
+ $('.card').each(function () {
+  if ($(this).isInViewport()) {
+   $('.card').addClass('scroll');
+  }
+ })
 })
